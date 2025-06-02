c51ed02bdb0773177b652c35d28ca47dfe26b4ae
Arjun <arjunsinghanya35@gmail.com>
2025-06-02 20:16:41 +0530

    day 2 jun 2025

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,14 +1,23 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar';
+import "./App.css"
+import {Routes,Route} from 'react-router-dom'
+import Home from "./pages/home/Home";
+import Cart from "./pages/cart/Cart"
+import PlaceOrder from './pages/placeOrder/PlaceOrder'
 
-import './App.css'
-
-function App() {
-
-
+const App = () => {
   return (
-    <>
-  <h1>hello</h1>
-    </>
+    <div className='app'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+        <Route path='/order' element={<PlaceOrder/>}/>
+      </Routes>
+      </div>
+
   )
 }
 
-export default App
+export default App;
---
+++ demo/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-content">
+            <h2>Order your favourite food here</h2>
+            <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredeants delectable array of dishtes crafted with the finest ingredeant and culinary experties .satistisy you cravings and elevate your dining experience, one delicious meal at the time</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
